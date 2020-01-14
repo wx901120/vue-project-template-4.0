e47f6036c6fad7188574a384a035cbe52ce8e85b
wangxing <wangxing@countrygarden.com.cn>
2020-01-14 15:34:15 +0800

    经典圣杯布局

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,48 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <!-- 经典圣杯布局 -->
+  <div class="layout">
+    <header>header</header>
+    <div class="main">
+      <nav class="main-nav">nav</nav>
+      <main class="main-content">main</main>
+      <aside class="main-aside">aside</aside>
+    </div>
+    <footer>footer</footer>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  components: {},
+  created() {},
+  mounted() {},
+  watch: {},
+  methods: {}
+};
+</script>
+<style lang="scss" scoped>
+.layout {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+header,
+footer {
+  flex: 0 0 2em; //1 1 0%
+  border: 1px solid red;
+}
+.main {
+  display: flex;
+  flex: 1;
+  .main-nav,
+  .main-aside {
+    flex: 0 0 12em;
+    border: 1px solid blue;
+  }
+  .main-content {
+    flex: 1;
+  }
+}
+</style>
